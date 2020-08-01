dcb3863e5403b326e7b734abe4e2bdbf4c3aa90d
Miek Messerschmidt <mmesserschmidt@gmail.com>
2020-08-01 13:54:31 +0200

    structure improved

--- frow/structure.py
+++ frow/structure.py
@@ -1,19 +1,28 @@
 import os
 import shutil
 import re
+import logging
 
+logger = logging.getLogger(__name__)
 
-def cp_structure_func(source_fn, dest_root):
+def dest_exists(source_fn, dest_root):
+    _, fn = os.path.split(source_fn)        
+    dest_fn = os.path.join(dest_root, fn)
+    return os.path.isfile(dest_root) or os.path.isfile(dest_fn)
+
+def cp_structure_func(source_fn, dest_root, override=True):    
+    if dest_exists(source_fn, dest_root) and not override:
+        raise FileExistsError(f"dest exists: {source_fn} -> {dest_root}")
     shutil.copy(source_fn, dest_root)
 
 
-def st_num_structure_func(source_fn, dest_root):
+def st_num_structure_func(source_fn, dest_root, override=True):
+
     st_num_regex = re.compile(r"u\d{8}")
 
     if not (found := st_num_regex.search(source_fn)):
         raise NameError(f"{source_fn} has no st_number")
 
-    print(found)
     sub_folder = found.group(0)
 
     base = os.path.join(dest_root, sub_folder)
@@ -21,13 +30,24 @@ def st_num_structure_func(source_fn, dest_root):
         os.makedirs(base)
     except FileExistsError:
         pass
-    
+
+    if dest_exists(source_fn, base) and not override:
+        raise FileExistsError(f"dest exists: {source_fn} -> {dest_root}")
+
+
     shutil.copy(source_fn, base)
 
 
-def bulk(source, dest, structure_func=cp_structure_func):
+def bulk(source, dest, structure_func=cp_structure_func, override=True, silent_errors=True):
 
     for root, dirs, files in os.walk(source):
         for fn in files:
-            source_fn = os.path.join(root, fn)
-            structure_func(source_fn, dest)
+            try:
+                source_fn = os.path.join(root, fn)
+                structure_func(source_fn, dest, override=override,)
+            except Exception as e:
+                logger.exception(f"Failed structuring {source_fn}", stack_info=True,)
+                if not silent_errors:
+                    raise
+                if isinstance(e, KeyboardInterrupt):
+                    raise
--- test/test_structure.py
+++ test/test_structure.py
@@ -11,25 +11,32 @@ fixture_path = "test/fixtures/structure"
 def test_cp_structure_func(tmp_path):
 
     fn = "crap_u43214322__asdf.txt"
-    source_path = os.path.join(fixture_path, fn)
+    source_path = os.path.join(fixture_path, "st_num",fn)
 
-    structure.cp_structure_func(source_path, tmp_path)
+    structure.cp_structure_func(source_path,  tmp_path)
 
     out_fn = os.path.join(tmp_path, fn)
-
     assert os.path.isfile(out_fn)
+
+
+    with pytest.raises(FileExistsError):
+        structure.cp_structure_func(source_path,  tmp_path, override=False)
+
 
 
 def test_st_num_structure_func(tmp_path):
 
     fn = "crap_u43214322__asdf.txt"
-    source_path = os.path.join(fixture_path, fn)
+    source_path = os.path.join(fixture_path, "st_num", fn)
 
     structure.st_num_structure_func(source_path, tmp_path)
 
     out_fn = os.path.join(tmp_path, "u43214322", fn)
-
     assert os.path.isfile(out_fn)
+
+    with pytest.raises(FileExistsError):
+        structure.st_num_structure_func(source_path, tmp_path, override=False)
+
 
 def test_bulk(tmp_path):
 
@@ -38,10 +45,30 @@ def test_bulk(tmp_path):
         "starting_u43214321__asdf.txt",
     ]
 
-    structure.bulk(fixture_path, tmp_path, structure_func=structure.st_num_structure_func)
+    source_path = os.path.join(fixture_path, "st_num")
+    structure.bulk(source_path, tmp_path, structure_func=structure.st_num_structure_func, silent_errors=False)
 
     out_fn = os.path.join(tmp_path, "u43214322", fns[0])
     assert os.path.isfile(out_fn)
 
     out_fn = os.path.join(tmp_path, "u43214321", fns[1])
     assert os.path.isfile(out_fn)
+
+
+def test_bulk_no_override(tmp_path):
+
+
+    source_path = os.path.join(fixture_path, "st_num")
+    structure.bulk(source_path, tmp_path, structure_func=structure.st_num_structure_func, silent_errors=False)
+
+    with pytest.raises(FileExistsError):
+        structure.bulk(source_path, tmp_path, structure_func=structure.st_num_structure_func, silent_errors=False, override=False)
+
+
+def test_bulk_errors(tmp_path):
+
+    source_path = os.path.join(fixture_path, "st_num_fail")
+
+    with pytest.raises(NameError):
+        structure.bulk(source_path, tmp_path, structure_func=structure.st_num_structure_func, silent_errors=False)
+
