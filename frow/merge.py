23b8740b926ab949846e00a942a0c4674a3a949c
Miek Messerschmidt <mmesserschmidt@gmail.com>
2020-08-01 14:01:50 +0200

    merge logging and errors

--- frow/merge.py
+++ frow/merge.py
@@ -5,18 +5,28 @@ import shutil
 import tqdm
 import pathlib
 
-def merge(source_list, dest):
+import logging
+
+logger = logging.getLogger(__name__)
+
+def merge(source_list, dest, override = True):
+
+    if os.path.isfile(dest) and not override:
+        raise FileExistsError(f"{dest} exists")
+
     b = fitz.open()
 
     for source_fn in source_list:
         a = fitz.open(source_fn)
         b.insertPDF(a)
 
+    
     b.save(dest)
+    
 
 
 
-def subfolder_merge(source, dest):
+def subfolder_merge(source, dest, silent_errors=False, override=True):
     for root, dirs, files in os.walk(source):
 
         if not files:
@@ -28,4 +38,11 @@ def subfolder_merge(source, dest):
         parent = os.path.split(root)[-1]
         dest_fn = os.path.join(dest, f"{parent}.pdf")
 
-        merge(sources, dest_fn)
+        try:
+            merge(sources, dest_fn, override=override)
+        except Exception as e:
+            logger.exception(f"Failed merging {sources} into {dest_fn}", stack_info=True)
+            if not silent_errors:
+                raise e
+            if silent_errors and isinstance(e, KeyboardInterrupt):
+                raise e
--- test/test_merge.py
+++ test/test_merge.py
@@ -23,6 +23,19 @@ def test_merge(tmp_path):
     assert A.pageCount == 5
 
 
+def test_merge_no_override(tmp_path):
+
+    L = [
+        os.path.join(sub_folder_merge_path, "a", "0.pdf"),
+        os.path.join(sub_folder_merge_path, "a", "1.pdf")
+    ]
+    out = os.path.join(tmp_path, "a.pdf")
+    merge.merge(L, out)
+    
+    with pytest.raises(FileExistsError):
+        merge.merge(L, out, override=False)
+
+
 def test_subfolder_merge(tmp_path):
 
     merge.subfolder_merge(sub_folder_merge_path, tmp_path)
@@ -38,4 +51,12 @@ def test_subfolder_merge(tmp_path):
     assert B.pageCount == 3
 
     
+def test_subfolder_merge_no_override(tmp_path):
 
+    merge.subfolder_merge(sub_folder_merge_path, tmp_path)
+
+    with pytest.raises(FileExistsError):
+        merge.subfolder_merge(sub_folder_merge_path, tmp_path, override=False)
+
+
+    
