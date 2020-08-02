eeeb4ca68474f0921e339d6ce3c6c274e0900121
Miek Messerschmidt <mmesserschmidt@gmail.com>
2020-08-02 14:51:37 +0200

    add page identifiers

---
+++ frow/tasks/tasks.py
@@ -0,0 +1,28 @@
+import json
+from ..tools import pdf_transform, qr
+
+
+def page_data(doc_id, page_index, total_pages):
+    return json.dumps({
+        "type":"page_id_mark",
+        "doc_id" : doc_id,        
+        "page_index" : page_index,
+        "total_pages" : total_pages
+    })
+
+
+def add_page_id_marks(fitz_doc, paste_relative_rect, doc_id, page_data_gen = page_data, qr_kwargs = {}, paste_kwargs={}):
+
+    for i, page in enumerate(fitz_doc.pages()):        
+        data = page_data_gen(doc_id, i, total_pages=fitz_doc.pageCount)
+        qr_pdf = qr.qr_pdf( data, **qr_kwargs)
+        # qr_pdf.save(f"/tmp/id_mark{i}.pdf")
+        
+        pdf_transform.paste_pdf_on(page, qr_pdf, relative_rect=paste_relative_rect, **paste_kwargs)
+
+    return fitz_doc
+
+
+
+
+
---
+++ test/tasks/test_add_page_id_marks.py
@@ -0,0 +1,24 @@
+
+import subprocess
+import fitz
+import os
+import pytest
+import frow.tasks.tasks as tasks
+
+
+import pytest
+
+def test_add_page_id_marks(tmp_path):
+
+    fn = os.path.join("test/fixtures/sub_folder_merge", "a", "0.pdf")
+    out = os.path.join(tmp_path, "0.pdf")
+
+    doc = fitz.open(fn)
+
+    tasks.add_page_id_marks(doc, (.8,.9,.95,.99), doc_id = "4444")
+
+
+    doc.save(out)
+
+    subprocess.call(["xdg-open", out])
+
