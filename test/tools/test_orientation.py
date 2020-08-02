80b8647bc4b3c27debccdf882a23e0f0dc4f5817
Miek Messerschmidt <mmesserschmidt@gmail.com>
2020-08-02 16:17:11 +0200

    orientation

--- test/tools/test_orientation.py
+++ test/tools/test_orientation.py
@@ -4,14 +4,19 @@ from frow.tools import orientation
 
 fixture_path = "test/fixtures/orientation"
 
-def test_orientation_vec_from_qr():
 
-    fn = "test/fixtures/orientation/tr.pdf"
+@pytest.mark.parametrize("fn, expected", [
+    (os.path.join(fixture_path, "tl.pdf"), (1,0,0,0)),
+    (os.path.join(fixture_path, "tr.pdf"), (0,1,0,0)),
+    (os.path.join(fixture_path, "bl.pdf"), (0,0,1,0)),
+    (os.path.join(fixture_path, "br.pdf"), (0,0,0,1)),
+])
+def test_orientation_vec_from_qr(fn, expected):
+
 
     doc = fitz.open(fn)
     v = orientation.orientation_vector_from_qr(doc[0],)
 
-    expected = (0,1,0,0)
 
     assert (v == expected).all()
 
