2939afb680a39febf1eb88bf8add70a3b8a8efe3
Miek Messerschmidt <mmesserschmidt@gmail.com>
2021-04-05 19:53:36 +0200

    setup includes data files

--- setup.py
+++ setup.py
@@ -13,9 +13,10 @@ with pathlib.Path('requirements.txt').open() as requirements_txt:
 
 setup(
     name='frow',
-    version='1.2.0',
+    version='1.3.0',
     packages=find_packages(),    
     install_requires=install_requires,
     
     include_package_data = True,
+    package_data={'frow': ['suite/*.pdf']},
 )
